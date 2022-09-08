886631778ab0d5ae48d3fb75ed50422b7c58033e
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2022-09-08 17:55:27 +0300

    optimizations + time calc

--- Main.cs
+++ Main.cs
@@ -3,59 +3,87 @@
 class Programm
 {
     static DateTime customTime = new DateTime();
+    static TimeSpan customTimeSpan = new TimeSpan();
     static void Main(string[] args)
     {
-        var currentTime = DateTime.Now;
-        Console.WriteLine("Current time: " + currentTime.ToString("\tdddd, dd MMMM yyyy HH:mm:ss"));
-        Console.WriteLine("Short variant: \t" + currentTime);
-        Console.WriteLine("Time zone: \t" + currentTime.ToString("GMT K"));
         MenuSwitcher();
-
     }
 
     static void MenuSwitcher()
     {
-        Console.WriteLine("\n\n Time menu:");
-        Console.WriteLine("1.Validate your time");
-        Console.WriteLine("2.Substract your time to current time");
-        Console.WriteLine("3.Substract your time to new time");
+        Console.WriteLine("Time menu:");
+        Console.WriteLine("1.Show current time");
+        Console.WriteLine("2.Validate your time");
+        Console.WriteLine("3.Substract your time with current time");
+        Console.WriteLine("4.Add time to time");
+        Console.WriteLine("5.Subtract time to time");
+        Console.WriteLine();
 
         switch (Console.ReadLine())
         {
             case "1":
-                ValidateDateTime();
+                ShowCurrentTime();
                 break;
             case "2":
-                if(ValidateDateTime())
-                    CalculateTime();
+                ValidateDateTime();
                 break;
             case "3":
+                if(ValidateDateTime())
+                    SubtractTime();
+                break;
+            case "4":
                 if (ValidateDateTime())
-                    CalculateTime();
+                    CalculateTime('+');
+                break;
+            case "5":
+                if (ValidateDateTime())
+                    CalculateTime('-');
                 break;
             default:
                 Console.WriteLine("Wrong menu number.");
-                MenuSwitcher();
                 break;
         }
+        MenuSwitcher();
     }
 
     static bool ValidateDateTime()
     {
         Console.WriteLine("Enter your time...");
-        string timeStr = Console.ReadLine();
-        if (!DateTime.TryParse(timeStr, out customTime))
+        if (!DateTime.TryParse(Console.ReadLine(), out customTime))
         {
             Console.WriteLine("DateTime isn't correct");
             return false;
         }
         Console.WriteLine("Time is correct. (" + customTime + ")");
+        Console.WriteLine();
         return true;
     }
 
-    static void CalculateTime()
+    static void SubtractTime()
     {
-        TimeSpan timeSpan = customTime.Subtract(DateTime.Now);
-        Console.WriteLine("("+ customTime + ") - (" + DateTime.Now + ") = " + timeSpan.Days + " days " + timeSpan.Hours + ":" + timeSpan.Minutes + ":" + timeSpan.Seconds);
+        customTimeSpan = customTime.Subtract(DateTime.Now);
+        string timeFormatted = (customTimeSpan.Hours.ToString() + ':' + customTimeSpan.Minutes.ToString() + ':' + customTimeSpan.Seconds.ToString()).Replace("-", "");
+        Console.WriteLine("Result is: ("+ customTime + ") - (" + DateTime.Now + ") = " + customTimeSpan.Days + " days " + timeFormatted);
+        Console.WriteLine("Result saved for further calculations.");
+        Console.WriteLine();
+    }
+
+    static void CalculateTime(char symbol)
+    {
+        if (symbol == '+')
+            Console.WriteLine("Result is: " + customTime.Add(customTimeSpan));
+        else
+            Console.WriteLine("Result is: " + customTime.Subtract(customTime));
+
+        Console.WriteLine();
+    }
+
+    static void ShowCurrentTime()
+    {
+        var currentTime = DateTime.Now;
+        Console.WriteLine("Current time: " + currentTime.ToString("\tdddd, dd MMMM yyyy HH:mm:ss"));
+        Console.WriteLine("Short variant: \t" + currentTime);
+        Console.WriteLine("Time zone: \t" + currentTime.ToString("GMT K"));
+        Console.WriteLine();
     }
 }
