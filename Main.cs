4019f4146f460e4c18d23d3fa2c91e051546a22c
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2022-09-09 12:31:47 +0300

    full time calculations and detection ready

--- Main.cs
+++ Main.cs
@@ -10,43 +10,56 @@
 
     static void TimeMenuSwitcher()
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Time menu:");
-        Console.WriteLine("1.Show current time");
-        Console.WriteLine("2.Validate your time");
-        Console.WriteLine("3.Substract your time with another time");
-        Console.WriteLine("4.Add time to time");
-        Console.WriteLine("5.Subtract time to time\n");
-
-        switch (Console.ReadLine())
+        try
         {
-            case "1":
-                Console.Clear();
-                ShowCurrentTime();
-                break;
-            case "2":
-                Console.Clear();
-                ValidateDateTime();
-                break;
-            case "3":
-                Console.Clear();
-                SubtractTime();
-                break;
-            case "4":
-                Console.Clear();
-                if (ValidateDateTime())
-                    CalculateTime('+');
-                break;
-            case "5":
-                Console.Clear();
-                if (ValidateDateTime())
-                    CalculateTime('-');
-                break;
-            default:
-                Console.WriteLine("Wrong menu number.");
-                break;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Time menu:");
+            Console.WriteLine("1.Show current time"); 
+            Console.WriteLine("2.Validate your time"); 
+            Console.WriteLine("3.Substract your time with another time");
+            Console.WriteLine("4.Add time to time");
+            Console.WriteLine("5.Subtract time from time");
+            Console.WriteLine("6.Find day by date");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Clear();
+                    ShowCurrentTime();
+                    break;
+                case "2":
+                    Console.Clear();
+                    ValidateDateTime();
+                    break;
+                case "3":
+                    Console.Clear();
+                    SubtractTime();
+                    break;
+                case "4":
+                    Console.Clear();
+                    if (ValidateDateTime())
+                        CalculateTime('+');
+                    break;
+                case "5":
+                    Console.Clear();
+                    if (ValidateDateTime())
+                        CalculateTime('-');
+                    break;
+                case "6":
+                    Console.Clear();
+                    if (ValidateDateTime())
+                        Console.WriteLine("Full date is: " + firstCustomTime.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+                    break;
+                default:
+                    Console.WriteLine("Wrong menu number.");
+                    break;
+            }
+            TimeMenuSwitcher();
         }
-        TimeMenuSwitcher();
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex);
+        }
     }
 
     static bool ValidateDateTime(bool isFirstTime = true)
@@ -78,7 +91,7 @@
             var timeNow = DateTime.Now;
             customTimeSpan = firstCustomTime.Subtract(timeNow);
             string timeFormatted = (customTimeSpan.Hours.ToString() + ':' + customTimeSpan.Minutes.ToString() + ':' + customTimeSpan.Seconds.ToString()).Replace("-", "");
-            Console.WriteLine("Result is: (" + firstCustomTime + ") - (" + timeNow + ") = " + customTimeSpan.Days + " days " + timeFormatted);
+            Console.WriteLine($"Result is: ({firstCustomTime}) - ({timeNow}) = {customTimeSpan.Days} days " + timeFormatted);
             Console.WriteLine("Result saved for further calculations.\n");
         }
         else if (result == "n")
@@ -89,7 +102,7 @@
 
             customTimeSpan = firstCustomTime.Subtract(secondCustomTime);
             string timeFormatted = (customTimeSpan.Hours.ToString() + ':' + customTimeSpan.Minutes.ToString() + ':' + customTimeSpan.Seconds.ToString()).Replace("-", "");
-            Console.WriteLine("Result is: (" + firstCustomTime + ") - (" + secondCustomTime + ") = " + customTimeSpan.Days + " days " + timeFormatted);
+            Console.WriteLine($"Result is: ({firstCustomTime}) - ({secondCustomTime}) = {customTimeSpan.Days} days " + timeFormatted);
             Console.WriteLine("Result saved for further calculations.\n");
         }
         else
@@ -119,17 +132,120 @@
                 Console.WriteLine("h = " + customTimeSpan.TotalHours);
                 Console.WriteLine("m = " + customTimeSpan.TotalMinutes);
                 Console.WriteLine("s = " + customTimeSpan.TotalSeconds);
+                Console.WriteLine();
                 break;
         }
     }
 
     static void CalculateTime(char symbol)
     {
-        if (symbol == '+')
-            Console.WriteLine("Result is: " + firstCustomTime.Add(customTimeSpan)); // доделать калькуляцию времени
-        else
-            Console.WriteLine("Result is: " + firstCustomTime.Subtract(firstCustomTime));
+        Console.WriteLine("Do you want to use saved result, or a new one? saved/new");
+        float value = 0;
+        string timeFormat = "";
+        switch (Console.ReadLine())
+        {
+            case "saved":
+                Console.WriteLine("Using saved value from last subtraction: " + customTimeSpan);
+                timeFormat = "saved";
+                break;
+            case "new":
+                Console.WriteLine("Enter time format. (d - days, h - hours, m - minutes, s - seconds)");
+                timeFormat = Console.ReadLine();
+                switch (timeFormat)
+                {
+                    case "d":
+                        Console.WriteLine("Enter days amount:");
+                        if (!float.TryParse(Console.ReadLine(), out value))
+                        {
+                            Console.WriteLine("Incorrect input.");
+                            return;
+                        }
+                        break;
+                    case "h":
+                        Console.WriteLine("Enter hours amount:");
+                        if (!float.TryParse(Console.ReadLine(), out value))
+                        {
+                            Console.WriteLine("Incorrect input.");
+                            return;
+                        }
+                        break;
+                    case "m":
+                        Console.WriteLine("Enter minutes amount:");
+                        if (!float.TryParse(Console.ReadLine(), out value))
+                        {
+                            Console.WriteLine("Incorrect input.");
+                            return;
+                        }
+                        break;
+                    case "s":
+                        Console.WriteLine("Enter second amount:");
+                        if (!float.TryParse(Console.ReadLine(), out value))
+                        {
+                            Console.WriteLine("Incorrect input.");
+                            return;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect format.");
+                        return;
+                }
+                break;
+            default:
+                Console.WriteLine("Incorrect input.");
+                return;
+        }
 
+
+        if (symbol == '+')
+        {
+            switch (timeFormat)
+            {
+                case "d":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddDays(value));
+                    break;
+                case "h":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddHours(value));
+                    break;
+                case "m":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddMinutes(value));
+                    break;
+                case "s":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddSeconds(value));
+                    break;
+                case "saved":
+                    
+                    Console.WriteLine("Result is: " + firstCustomTime.AddDays(customTimeSpan.TotalDays));
+                    break;
+                default:
+                    Console.WriteLine("Idk how did u get here :3");
+                    break;
+            }
+        }
+        else
+        {
+            switch (timeFormat)
+            {
+                case "d":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddDays(-value));
+                    break;
+                case "h":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddHours(-value));
+                    break;
+                case "m":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddMinutes(-value));
+                    break;
+                case "s":
+                    Console.WriteLine("Result is: " + firstCustomTime.AddSeconds(-value));
+                    break;
+                case "saved":
+
+                    Console.WriteLine("Result is: " + firstCustomTime.AddDays(-customTimeSpan.TotalDays));
+                    break;
+                default:
+                    Console.WriteLine("Idk how did u get here :3");
+                    break;
+            }
+        }
         Console.WriteLine();
     }
 
@@ -141,4 +257,5 @@
         Console.WriteLine("Time zone: \t" + currentTime.ToString("GMT K"));
         Console.WriteLine();
     }
+
 }
