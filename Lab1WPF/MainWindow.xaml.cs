e25f6dd887cccd0f648be699527fd920ee0f8ed3
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2023-02-14 13:52:29 +0200

    added two windows

--- Lab1WPF/MainWindow.xaml.cs
+++ Lab1WPF/MainWindow.xaml.cs
@@ -9,15 +9,22 @@ namespace Lab1WPF
         {
             InitializeComponent();
         }
+        private void SortManagerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // add new window opening with  SortManager.SortMenuSwitcher();
+            SortManagerWindow sortManagerWindow = new SortManagerWindow();
+            sortManagerWindow.Closed += (object? sender, System.EventArgs e) => { this.Visibility = Visibility.Visible; };
+            this.Visibility = Visibility.Hidden;
+            sortManagerWindow.Show();
+        }
 
         private void TimeManagerBtn_Click(object sender, RoutedEventArgs e)
         {
             // add new window opening with  TimeManager.TimeMenuSwitcher();
-        }
-
-        private void SortManagerBtn_Click(object sender, RoutedEventArgs e)
-        {
-            // add new window opening with  SortManager.SortMenuSwitcher();
+            TimeManagerWindow timeManagerWindow = new TimeManagerWindow();
+            timeManagerWindow.Closed += (object? sender, System.EventArgs e) => { this.Visibility = Visibility.Visible; };
+            this.Visibility = Visibility.Hidden;
+            timeManagerWindow.Show();
         }
     }
 }
