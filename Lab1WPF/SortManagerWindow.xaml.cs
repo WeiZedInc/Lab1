af84a89ffb4a8d2dca8e232dfc0cc8ac8673e512
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2023-02-21 14:45:29 +0200

    sort manager window improvements

--- Lab1WPF/SortManagerWindow.xaml.cs
+++ Lab1WPF/SortManagerWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Lab1WPF
 {
@@ -10,6 +11,30 @@ namespace Lab1WPF
         public SortManagerWindow()
         {
             InitializeComponent();
+        }
+
+        private void GetResult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HowToUseButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Firstly, choose what do You want to sort:\n" +
+                "1.Array<T>\n" +
+                "2.List<T>\n" +
+                "3.LinkedList<T>\n\n" +
+                "Than choose the type (T) for desired array:\n" +
+                "1.int\n" +
+                "2.float\n" +
+                "3.double\n" +
+                "4.short\n" +
+                "5.ushort\n" +
+                "6.uint\n\n" +
+                "Than choose the sorting alghoritm.\n" +
+                "Afterall input values to the left input box and click Result button.\n\n" +
+                "If You want to sort array of floating values,\nuse the WHITESPACE separator, example [23,4 -23,431 0,13f]\n\n" +
+                "If You want to sort array of integers,\nuse ',' or whitespace or ';' separators", "How to use soting");
         }
     }
 }
